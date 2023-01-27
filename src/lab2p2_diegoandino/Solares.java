a4b2d9052f003df1e85bc5192343ba5195e22ccb
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-01-27 14:56:58 -0600

    Crear clase Solares

--- src/lab2p2_diegoandino/Solares.java
+++ src/lab2p2_diegoandino/Solares.java
@@ -2,5 +2,55 @@
 package lab2p2_diegoandino;
 
 public class Solares {
-    //Largo ancho  area dueno
+    
+    private int largo;
+    private int ancho;
+    private int area;
+    private String dueño;
+    
+    public Solares(int largo, int ancho, int area, String dueño){
+        this.largo = largo;
+        this.ancho = ancho;
+        this.area = area;
+        this.dueño = dueño;  
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public String getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(String dueño) {
+        this.dueño = dueño;
+    }
+    
+    public String toString(){
+        return "Largo: " + largo + "Ancho: " + ancho + 
+                "Area: " + area + "Dueño: " + dueño;
+    }
+   
+    
 }
