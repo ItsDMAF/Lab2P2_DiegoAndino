11da5ddaa1d7dc0b22fdbf1434dbd9b7f0f318b4
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-01-27 14:31:34 -0600

    Crear clase Users

--- src/lab2p2_diegoandino/Casas.java
+++ src/lab2p2_diegoandino/Casas.java
@@ -3,4 +3,7 @@ package lab2p2_diegoandino;
 
 public class Casas {
     
+    private String casa;
+    private int bloque;
+    private 
 }
--- src/lab2p2_diegoandino/Solares.java
+++ src/lab2p2_diegoandino/Solares.java
@@ -2,5 +2,5 @@
 package lab2p2_diegoandino;
 
 public class Solares {
-    
+    //Largo ancho  area dueno
 }
